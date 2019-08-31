e6b285d52f48f280e91b8ca2dfdab44b2a17a2b3
Lucas Piergiacomi <lg.piergiacomi@gmail.com>
2019-08-31 12:32:22 -0300

    travis + springboot

---
+++ src/main/java/unq/tpi/desapp/DesappGroupc2fBackendApplication.java
@@ -0,0 +1,13 @@
+package unq.tpi.desapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesappGroupc2fBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesappGroupc2fBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/unq/tpi/desapp/DesappGroupc2fBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package unq.tpi.desapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesappGroupc2fBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
