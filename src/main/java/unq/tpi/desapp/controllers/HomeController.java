29fe19281d72053d67a22afae71ba745cdf3c9d9
Lucas Piergiacomi <lg.piergiacomi@gmail.com>
2019-09-03 14:07:02 -0300

    prueba heroku

--- src/main/java/unq/tpi/desapp/controllers/HomeController.java
+++ src/main/java/unq/tpi/desapp/controllers/HomeController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/v1")
+//@RequestMapping("/")
 public class HomeController {
 
 
-    @CrossOrigin(origins = "https://app-grupoc2f-022019.herokuapp.com")
+    //@CrossOrigin(origins = "https://app-grupoc2f-022019.herokuapp.com")
     @GetMapping("/home")
     public String index() {
         return "Hola mundo";
