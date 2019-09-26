1069d2defaea89a6cf8939fd5787d912629f056a
Esteban Matas <estebanmatas13@gmail.com>
2019-09-26 16:19:02 -0300

    Implementado Email

---
+++ src/main/java/unq/tpi/desapp/model/Email.java
@@ -0,0 +1,29 @@
+package unq.tpi.desapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Email {
+
+    private String user;
+    private String password;
+    private String subject;
+    private String receiver;
+    private String message;
+
+    public Email() {
+
+        this.user = "contacto.viandasya@gmail.com";
+        this.password = "viandas1234";
+
+    }
+
+    public void createEmailWith(String subject, String receiver, String message) {
+        setSubject(subject);
+        setReceiver(receiver);
+        setMessage(message);
+    }
+
+}
