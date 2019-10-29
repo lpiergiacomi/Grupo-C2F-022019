51af7c41b4228431bda35d22cc4a4073b3f5a2cf
Lucas Piergiacomi <lg.piergiacomi@gmail.com>
2019-10-29 19:33:11 -0300

    cors

--- src/main/java/unq/tpi/desapp/controllers/HomeController.java
+++ src/main/java/unq/tpi/desapp/controllers/HomeController.java
@@ -1,12 +1,13 @@
 package unq.tpi.desapp.controllers;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import unq.tpi.desapp.model.Provider;
 import unq.tpi.desapp.persistence.ProviderRepository;
 
-
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 public class HomeController {
 
